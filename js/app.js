b23af4e266fb5cd324fa6899fd7283c25dca6bf3
Sam Hamm <samhamm@gmail.com>
2017-06-20 14:06:44 -0700

    end of monday work

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 Product.names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+
 Product.totalClicks = 0;
 
 Product.left = document.getElementById('left');
